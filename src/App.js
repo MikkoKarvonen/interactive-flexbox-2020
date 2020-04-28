690810e435b5016eef7737d839469c8df9f6382c
Mikko <mkarvonen34@gmail.com>
2020-04-28 17:07:11 +0300

    Add styles as json

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [fdStyle, setFdStyle] = useState("row");
+  const [fwStyle, setFwStyle] = useState("nowrap");
+  const [jcStyle, setJcStyle] = useState("flex-start");
+  const [aiStyle, setAiStyle] = useState("flex-start");
+
+  let shapesStyle = {
+    width: "400px",
+    height: "400px",
+    background: "#333333",
+    display: "flex",
+    flexDirection: fdStyle,
+    flexWrap: fwStyle,
+    justifyContent: jcStyle,
+    alignItems: aiStyle,
+  };
+
   return (
     <div>
-      <div className="shapes">
-        <div className="shape1">｡◕‿‿◕｡</div>
-        <div className="shape2">ಥ﹏ಥ</div>
-        <div className="shape3">ᵔᴥᵔ</div>
-        <div className="shape4">ಠ╭╮ಠ</div>
-        <div className="shape5">˘▾˘</div>
+      <div style={shapesStyle}>
+        <div className="shape shape1">｡◕‿‿◕｡</div>
+        <div className="shape shape2">ಥ﹏ಥ</div>
+        <div className="shape shape3">ᵔᴥᵔ</div>
+        <div className="shape shape4">ಠ╭╮ಠ</div>
+        <div className="shape shape5">˘▾˘</div>
       </div>
     </div>
   );
