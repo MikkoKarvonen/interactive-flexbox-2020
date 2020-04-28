eeef9c3234092a458d6676c2f874b40f237567fb
Mikko <mkarvonen34@gmail.com>
2020-04-28 17:27:32 +0300

    Add code for flexbox

--- src/App.js
+++ src/App.js
@@ -45,31 +45,53 @@ function App() {
       </div>
 
       <div id="controls">
-        <p class="label primary">Flex-direction</p>
+        <p>Flex-direction</p>
         <button onClick={() => fdChange("row")}>row</button>
         <button onClick={() => fdChange("row-reverse")}>row-reverse</button>
         <button onClick={() => fdChange("column")}>column</button>
         <button onClick={() => fdChange("column-reverse")}>
           column-reverse
         </button>
-        <p class="label secondary">Flex-wrap</p>
+        <p>Flex-wrap</p>
         <button onClick={() => fwChange("nowrap")}>nowrap</button>
         <button onClick={() => fwChange("wrap")}>wrap</button>
         <button onClick={() => fwChange("wrap-reverse")}>wrap-reverse</button>
-        <p class="label success">Justify-content</p>
+        <p>Justify-content</p>
         <button onClick={() => jcChange("flex-start")}>flex-start</button>
         <button onClick={() => jcChange("flex-end")}>flex-end</button>
         <button onClick={() => jcChange("center")}>center</button>
         <button onClick={() => jcChange("space-between")}>space-between</button>
         <button onClick={() => jcChange("space-around")}>space-around</button>
         <button onClick={() => jcChange("space-evenly")}>space-evenly</button>
-        <p class="label warning">Align-items</p>
+        <p>Align-items</p>
         <button onClick={() => aiChange("flex-start")}>flex-start</button>
         <button onClick={() => aiChange("flex-end")}>flex-end</button>
         <button onClick={() => aiChange("center")}>center</button>
         <button onClick={() => aiChange("baseline")}>baseline</button>
         <button onClick={() => aiChange("stretch")}>stretch</button>
       </div>
+
+      <code>
+        <div>
+          display: <span>flex</span>;
+        </div>
+        <div>
+          flex-direction:
+          <span id="flex_direction_style">{fdStyle}</span>;
+        </div>
+        <div>
+          flex-wrap:
+          <span id="flex_wrap_style">{fwStyle}</span>;
+        </div>
+        <div>
+          justify-content:
+          <span id="justify_content_style">{jcStyle}</span>;
+        </div>
+        <div>
+          align-items:
+          <span id="align_items_style">{aiStyle}</span>;
+        </div>
+      </code>
     </div>
   );
 }
